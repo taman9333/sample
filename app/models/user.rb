db6ed141fc5424c8e765abf654f9f9b7449a5f46
taman9333 <abdelrahman.taman@robustastudio.com>
2018-09-01 18:37:19 +0200

    downcase all emails before saving to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+
+  befoer_save :downcase_email
+
   validates :username, presence: true,
             uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, length: { maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  def downcase_email
+    email = email.downcase
+  end
+
 end
